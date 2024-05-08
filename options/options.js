6008d317620ba7d4b524e139e3a281dee4b7facc
Crowdbotics <engineering@crowdbotics.com>
2024-05-08 18:15:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tst-addaccountexpa-135170",
+  url: "https://tst-addaccountexpa-135170.botics.co",
+  api: "https://tst-addaccountexpa-135170.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
